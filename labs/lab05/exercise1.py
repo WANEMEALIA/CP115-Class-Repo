b6d48eb84e2403f44d78f09f87252f35ecc39bca
WANEMEALIA <nwanemealia@gmail.com>
2025-08-04 12:00:22 +0800

    update in lab 05

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,13 @@
+# Basic input and output
+print("Welcome to the Student Information System!")
+
+# Getting user input (always returns a string)
+student_name = input("Enter your name: ")
+student_age = input("Enter your age: ")
+course_code = input("Enter the course code: ")
+
+# Formatted output using string concatenation
+print()
+print("Student Name: " + student_name)
+print("Age: " + student_age)
+print("Course code: " + course_code)
