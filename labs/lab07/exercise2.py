fb944e3ef4135a8700b8788b8f0a3b03b4a2b932
WANEMEALIA <nwanemealia@gmail.com>
2025-08-18 21:58:19 +0800

    update in lab07

--- labs/lab07/exercise2.py
+++
@@ -1,9 +0,0 @@
-# Convert these to f-strings
-product_name = "Laptop"
-price = 1299.99
-quantity = 2
-total = price * quantity
-print(f"Product: {product_name}")
-print(f"Price: ${price}")
-print(f"Quantity: {quantity}")
-print(f"Total: ${total}")
