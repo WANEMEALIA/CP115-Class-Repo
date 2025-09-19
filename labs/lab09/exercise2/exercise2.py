2a13f88a7868fce3406131bcf34240e879085189
WANEMEALIA <nwanemealia@gmail.com>
2025-09-19 14:28:58 +0800

    update in lab 09

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -2,34 +2,49 @@
 #Input
 employee_name = input("Enter your name: ")
 base_salary = float(input("Enter your base salary: "))
-overtime_hours = int(input("Enter your overtime hours: "))
+ot_hours = int(input("Enter your overtime hours: "))
 tax_status = input("Enter your status: ")
 
-total_income = base_salary + (overtime_hours * 35)
+#constant
+ot = 35
 epf = 0.11 
 socso = 0.005
 
-# TODO your code here
-if (tax_status == "Single") and (total_income >= 5000) :
-    tax_rate = "22%"
-    tax = 0.22
-else :
-    tax_rate = "18%"
-    tax = 0.18
-if (tax_status == "Married") and (total_income >= 6000) :
-    tax_rate = "20%"
-    tax = 0.2
-else :
-    tax_rate = "15%"
-    tax = 0.15
-if (tax_status == "Head") and (total_income >= 5500) :
-    tax_rate = "25%"
-    tax = 0.25
-else :
-    tax_rate = "19%"
-    tax = 0.19
+#Calculate ot pay
+ot_pay = ot_hours * ot
 
-net_salary = total_income - ((tax*total_income)+(epf*total_income)+(socso*total_income))
+#Total income bfore deducted
+total_income = base_salary + ot_pay
+
+#TODO your code here
+if (tax_status == "Single") :
+    if (total_income >= 5000) :
+        tax_rate = "22%"
+        tax = 0.22
+    else :
+        tax_rate = "18%"
+        tax = 0.18
+elif (tax_status == "Married") :
+    if (total_income >= 6000) :
+        tax_rate = "20%"
+        tax = 0.2
+    else :
+        tax_rate = "15%"
+        tax = 0.15
+elif (tax_status == "Head") :
+    if (total_income >= 5500) :
+        tax_rate = "25%"
+        tax = 0.25
+    else :
+        tax_rate = "19%"
+        tax = 0.19
+
+after_tax = total_income * (1-tax)
+
+epf_deduction = total_income*epf
+socso_deduction = total_income*socso
+
+net_salary = after_tax - epf_deduction - socso_deduction
 
 #Output
 print(employee_name)
