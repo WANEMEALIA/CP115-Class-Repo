d902aef4ac4ffe92f233ac0683272158d0bac950
WANEMEALIA <nwanemealia@gmail.com>
2025-09-22 12:22:26 +0800

    update in lab 10

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -2,7 +2,24 @@ position = input()
 overtime_hours = int(input())
 is_weekend = input()
 
+bonus = 5
+overtime_rate = 1.5
+total_pay = 0
+overtime_pay = 0
+
 # Your code here
+if is_weekend == "Yes" :
+    overtime_pay = (overtime_hours*5) 
+    if position == "Manager" :
+        hourly_rate = 35
+    elif position == "Supervisor" :
+        hourly_rate = 25
+    else :
+        hourly_rate = 18
+else : 
+    overtime_pay = total_pay
+    
+total_pay = overtime_pay + hourly_rate
 
 print(hourly_rate)
 print(overtime_pay)
