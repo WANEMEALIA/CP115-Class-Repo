b220f98fb6262a95eaa09eb6a8e1c3e59f019012
WANEMEALIA <nwanemealia@gmail.com>
2025-09-22 12:25:54 +0800

    update in lab 10

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -12,7 +12,7 @@ if is_weekend == "Yes" :
     overtime_pay = (overtime_hours*5) 
     if position == "Manager" :
         hourly_rate = 35
-    elif position == "Supervisor" :
+    if position == "Supervisor" :
         hourly_rate = 25
     else :
         hourly_rate = 18
