a74b7b09b408340d7d5f81e883379f6cdf66e136
WANEMEALIA <nwanemealia@gmail.com>
2025-10-27 13:51:13 +0800

    update in lab 13

--- labs/lab13/exercise3/exercise3.py
+++ labs/lab13/exercise3/exercise3.py
@@ -1,21 +1,23 @@
+grade = float(input("Enter grade : "))
 valid_count = 0
 total = 0
+average = 0
 
-# TODO: Your code here
-while True:
-    grade = float(input())
-    
-    # Stop reading if a negative number is entered
-    if grade < 0:
-       continue
+#TODO: Your code here
+while grade > 0:
+    grade = float(input("Enter grade : "))
     valid_count += 1
     total += grade
+
+    if grade < 0:
+       print("Enter valid grade.")
+       continue
 
 # Avoid division by zero
     if valid_count > 0:
         average = total / valid_count
     else:
-        average = 0.0
+        average = 0
 
 print(valid_count)
-print(f"{average:.2f}")
+print(f"Grade average is : {average:.2f}")
